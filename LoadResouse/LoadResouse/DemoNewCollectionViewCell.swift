3a043fc4f1cab21fff20011a064bec7175863f25
zhugy <zhugy781@163.com>
2017-05-27 13:14:31 +0800

    addfile

---
+++ LoadResouse/LoadResouse/DemoNewCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DemoNewCollectionViewCell.swift
+//  LoadResouse
+//
+//  Created by zhugy on 2017/5/27.
+//  Copyright © 2017年 Zhugy. All rights reserved.
+//
+
+import UIKit
+
+class DemoNewCollectionViewCell: UICollectionViewCell {
+    
+}
